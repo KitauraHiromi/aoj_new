fc6b8469d2e7c8932a3538d36b8549751b7a5154
KitauraHiromi <kasumiga_eria394@yahoo.co.jp>
2016-08-10 23:41:44 +0900

    not completed 8/10

--- Introduction/Library_of_Computational_Geometry/projection.cpp
+++ Introduction/Library_of_Computational_Geometry/projection.cpp
@@ -3,6 +3,7 @@
 #include <iostream>
 #include <stdio.h>
 #include <stdlib.h>
+#include <iomanip>
 using namespace std;
 typedef vector<double> VecD;
 #define rep(n) for(int i=0;i<n;i++)
@@ -28,7 +29,7 @@ public:
 	VecD point;
 };
 
-const double operator*(const Point &p0, const Point &p1){ return inner_product( p0.point.begin(), p1.point.begin(), p0.point.end(), 0); }
+const double operator*(const Point &p0, const Point &p1){ return inner_product( p0.point.begin(), p0.point.end(), p1.point.begin(), 0); }
 const Point operator*(const double a, const Point &p1){
 	VecD v;
 	for(VecD::const_iterator iter1=p1.point.begin();iter1!=p1.point.end();iter1++) v.PB(*iter1 * a);
@@ -60,28 +61,23 @@ const Point operator-(Point &p0, Point &p1){ return p0+(-p1); }
 const Point operator-(double a, Point &p1){ return a+(-p1); }
 const Point operator-(Point &p1, double a){return (-a)+p1;}
 
-const Point projection(const Point v1, const Point v2){
-	return (v1*v2) / (v1*v1) * v1;
+const Point projection(const Point &p1, const Point &p2){
+	// cout << p1*p2 << endl;
+	return ((p1*p2) / (p1*p1)) * p1;
 }
 
 int main(void){
-	VecD v0(3,1);
-	VecD v1(3,2);
-	Point p0(v0);
-	Point p1(v1);
-	Point p = p0+p1;
-	cout << p.point[0] << endl;
-/*
+
 	double xp0, yp0, xp1, yp1; cin>>xp0>>yp0>>xp1>>yp1;
 	int q; cin >> q;
 	VecD v0; v0.PB(xp0); v0.PB(yp0); Point p0(v0);
 	VecD v1; v1.PB(xp1); v1.PB(yp1); Point p1(v1);
 	p1 = p1+p0;
+	cout << p1.point[0] << " " << p1.point[1] << endl;
 	rep(q){
 		double xp2,yp2; cin>>xp2>>yp2;
 		VecD v2; v2.PB(xp2); v2.PB(yp2); Point p2(v2);
 		Point ret = projection(p1, p2);
-		cout << "%f %f", %(ret.point[0], ret.point[1]) << endl;;
+		cout << fixed << setprecision(10) << ret.point[0] << " " << fixed << setprecision(10) << ret.point[1] << endl;
 	}
-	*/
 }
